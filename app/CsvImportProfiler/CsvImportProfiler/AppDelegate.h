bd7e8ef24ac328386284b7c36ea9f916efec8330
dodikk <dodikk88.reg@gmail.com>
2013-06-23 09:12:16 +0300

    Created project stub

---
+++ app/CsvImportProfiler/CsvImportProfiler/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CsvImportProfiler
+//
+//  Created by Oleksandr Dodatko on 23/06/2013.
+//  Copyright (c) 2013 Oleksandr Dodatko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ app/CsvImportProfiler/CsvImportProfiler/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CsvImportProfiler
+//
+//  Created by Oleksandr Dodatko on 23/06/2013.
+//  Copyright (c) 2013 Oleksandr Dodatko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
